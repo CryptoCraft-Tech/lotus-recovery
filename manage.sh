a079cd7914cefe12de469935259b7b38d4bffc83
jyma <897694714@qq.com>
2023-08-31 17:23:14 +0800

    init

---
+++ manage.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+LOCKFILE="/storage/worker.lock"
+SECTORSID_LIST="sectorsid.list"
+WORKER_LIST="/storage/worker.list"
+RECOVERY_SCRIPT="/storage/run_recovery.sh"
+ALLOWCORES_LIST="allowcores.list"
+MAX_NUM=14
+
+i=1
+j=1
+
+update_all_nums() {
+    while read -r worker_ip; do
+        (
+            flock -x 200
+            tmux_count=$(ssh "$worker_ip" "tmux list-sessions | grep '^recovery' | wc -l" </dev/null)
+            awk -v ip="$worker_ip" -v count="$tmux_count" -F' ' '/IP/ && $2 == ip { $4=count } {print}' $WORKER_LIST >${WORKER_LIST}.tmp && mv ${WORKER_LIST}.tmp /storage13/worker1.list
+        ) 200>$LOCKFILE
+    done < <(awk '/IP/ {print $2}' $WORKER_LIST)
+}
+
+while read -r sectorid; do
+    update_all_nums
+    read -r ip num core <<<$(awk '/IP/ {ip=$2; num=$4; cores=""; for (i=6; i<=NF; i++) cores=(cores==""?"":cores" ")$i; print ip, num, cores}' $WORKER_LIST | sort -k2n | head -n1 | awk '{printf $1" "$2" "; for (i=3; i<=NF; i++) printf $i" "; print ""}')
+
+    while ((num >= MAX_NUM)); do
+        echo "---------full sleep----------"
+        sleep 60
+        update_all_nums
+        read -r ip num core <<<$(awk '/IP/ {ip=$2; num=$4; cores=""; for (i=6; i<=NF; i++) cores=(cores==""?"":cores" ")$i; print ip, num, cores}' $WORKER_LIST | sort -k2n | head -n1 | awk '{printf $1" "$2" "; for (i=3; i<=NF; i++) printf $i" "; print ""}')
+    done
+
+    coregroup=$(grep "$ip" $ALLOWCORES_LIST | awk '/IP/ {cores=""; for (i=4; i<=NF; i++) cores=cores" "$i; print cores}')
+
+    if [ -n "$core" ]; then
+        for c in $core; do
+            coregroup=${coregroup//$c/}
+        done
+    fi
+    endcore=$(echo "$coregroup" | awk '{print $1}')
+    (
+        flock -x 200
+        sed -i "/^IP $ip /s/$/ $endcore/" $WORKER_LIST
+    ) 200>$LOCKFILE
+    echo "-----$sectorid-----$ip-----$endcore-------$i------"
+    ssh "$ip" "tmux new-session -d -s recovery-$sectorid '$RECOVERY_SCRIPT $endcore $sectorid $i $ip  2>&1 | tee /storage/log/$sectorid.log'" </dev/null
+
+    sed -i '1d' $SECTORSID_LIST
+
+    ((i++))
+    if [ "$i" -eq 16 ]; then
+        i=1
+    fi
+    if [ "$j" -eq 3000 ]; then
+        break
+    fi
+    ((j++))
+
+    echo "------sleep------"
+    sleep 90
+
+done <$SECTORSID_LIST
---
+++ run_recovery.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#!/bin/bash
+
+CORES=$1
+SECTOR_ID=$2
+SID=$3
+IP=$4
+
+LOCKFILE="/storage1/worker1.lock"
+WORKER_LIST="/storage1/worker1.list"
+RECO_FAULTS_LIST="/storage/reco_faults.list"
+CUDANUM=$((SID % 2))
+
+export FIL_PROOFS_MAXIMIZE_CACHING=1
+export FIL_PROOFS_USE_MULTICORE_SDR=1
+export FIL_PROOFS_MULTICORE_SDR_PRODUCERS=1
+export FIL_PROOFS_PARENT_CACHE=/storage/filecoin-parents-reco
+export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1
+export FIL_PROOFS_USE_GPU_TREE_BUILDER=1
+export FIL_PROOFS_MULTICORE_SDR_CORES="${CORES}"
+
+env CUDA_VISIBLE_DEVICES=${CUDANUM} TMPDIR=/storage/recotmp${CUDANUM} numactl --physcpubind=10-15,26-31,42-47,58-63 lotus-miner sectors recover --recovery-metadata=/storage/sectors-f02228866.json --recovery-key=+q28qE44PC3NdoNKuIuIlHaoVX9sg5eT6dCJ9fwdTK0= --recovering-from=/mnt/data4 --recovering-to=/storage/recover/recovering-to --is-deal=true --by-unsealed=false ${SECTOR_ID}
+
+if [ -e "/storage/recover/recovering-to/cache/s-t08866-${SECTOR_ID}/t_aux" ]; then
+    mv "/storage/recover/recovering-to/cache/s-t08866-${SECTOR_ID}" "/storage${SID}/cache/"
+    mv "/storage/recover/recovering-to/sealed/s-t08866-${SECTOR_ID}" "/storage${SID}/sealed/"
+    mv "/storage/recover/recovering-to/unsealed/s-t08866-${SECTOR_ID}" "/storage${SID}/unsealed/"
+else
+    echo "${SECTOR_ID}" >>$RECO_FAULTS_LIST
+fi
+
+(
+    flock -x 200
+    awk -v ip="${IP}" -v cores="${CORES}" '{
+    if ($2 == ip) {
+        printf "%s %s %s %s %s", $1, $2, $3, $4, $5;
+        for (i = 6; i <= NF; i++) {
+            if ($i != cores) {
+                printf " %s", $i;
+            }
+        }
+        printf "\n";
+    } else {
+        print $0;
+    }
+}' $WORKER_LIST >${WORKER_LIST}.tmp && mv ${WORKER_LIST}.tmp $WORKER_LIST
+) 200>$LOCKFILE
+
+tmux kill-session -t recovery-"${SECTOR_ID}"
